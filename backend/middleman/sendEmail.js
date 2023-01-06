18710394a24d6194e8af5fa1e5a6275d09e2dc81
Tushar Bhardwaj <bhardwajt423@gmail.com>
2023-01-06 19:25:15 +0530

    Initial

--- backend/middleman/sendEmail.js
+++ backend/middleman/sendEmail.js
@@ -4,25 +4,25 @@ exports.send_Email = async (options) => {
 
 
 
-    var transport = nodemailer.createTransport({
-        host: "smtp.mailtrap.io",
-        port: 2525,
-        auth: {
-          user: "3ad7a4708f024e",
-          pass: "6be4786683af01"
-        }
-      }); 
+    // var transport = nodemailer.createTransport({
+    //     host: "smtp.mailtrap.io",
+    //     port: 2525,
+    //     auth: {
+    //       user: "3ad7a4708f024e",
+    //       pass: "6be4786683af01"
+    //     }
+    //   }); 
 
-    // const transport = nodemailer.createTransport({
+    const transport = nodemailer.createTransport({
         
-    //     host :  process.env.SMTP_HOST,
-    //     port : process.env.SMTP_PORT,
-    //     auth : {
-    //         user : process.env.SMTP_EMAIL,
-    //         pass : process.env.SMTP_PASSWORD,
-    //     },
-    //     service :  process.env.SMTP_SERVICE,
-    // });
+        host :  process.env.SMTP_HOST,
+        port : process.env.SMTP_PORT,
+        auth : {
+            user : process.env.SMTP_EMAIL,
+            pass : process.env.SMTP_PASSWORD,
+        },
+        service :  process.env.SMTP_SERVICE,
+    });
 
     const mailOptions = {
         from : process.env.SMTP_EMAIL,
